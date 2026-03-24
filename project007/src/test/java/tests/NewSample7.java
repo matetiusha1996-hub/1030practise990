4e590f953a5c84361d29fc903458fcf2ef70a44b
mateti <matetiusha1996@gmail.com>
2026-03-24 13:44:11 +0530

    7 case added

---
+++ project007/src/test/java/tests/NewSample7.java
@@ -0,0 +1,23 @@
+package tests;
+
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class NewSample7 {
+
+	@Test
+	public void test1() throws Exception {
+		ChromeOptions options = new ChromeOptions();
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),options);
+		driver.get("https://google.com/");
+		Thread.sleep(60000);
+		driver.quit();
+		System.out.println("test1");
+		
+	}
+
+}
